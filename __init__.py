2172d16417ebc841580deadf85b686a7a8c3049f
djkil <djkill2009@gmail.com>
2024-01-14 18:32:46 +0300

    debug nodes

--- __init__.py
+++ __init__.py
@@ -5,6 +5,41 @@ import numpy as np
 from PIL import Image
 import torch
 
+# Hack: string type that is always equal in not equal comparisons
+class AnyType(str):
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+
+# Our any instance wants to be a wildcard string
+any = AnyType("*")
+
+class AnyToString:
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+
+        return {
+            "required": {
+                "any": (any, {}),                                        },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "test"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "utils"
+
+    def test(self, any):
+        res = str(any)
+        return {"ui": {"tags": res}, "result": (res,)}
+
 class FaceAnalyze:
 
     def __init__(self):
@@ -63,8 +98,8 @@ class FaceCompare:
             },
         }
 
-    RETURN_TYPES = ("STRING",)
-    #RETURN_NAMES = ("image_output_name",)
+    RETURN_TYPES = ("BOOLEAN", "FLOAT", "STRING")
+    RETURN_NAMES = ("verified", "distance", "result")
 
     FUNCTION = "test"
 
@@ -80,7 +115,7 @@ class FaceCompare:
         objs = DeepFace.verify(tensor1[0], tensor2[0], distance_metric = metric)
         res = json.dumps(objs,)
         print(res)
-        return {"ui": {"tags": res}, "result": (res,)}
+        return objs["verified"], objs["distance"], res
 
 
 # A dictionary that contains all nodes you want to export with their names
@@ -88,10 +123,12 @@ class FaceCompare:
 NODE_CLASS_MAPPINGS = {
     "FaceAnalyze": FaceAnalyze,
     "FaceCompare": FaceCompare,
+    "AnyToString": AnyToString,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FaceAnalyze": "Facial Attribute Analysis",
-    "FaceCompare": "Face similarity"
+    "FaceCompare": "Face similarity",
+    "AnyToString": "Any To String",
 }
