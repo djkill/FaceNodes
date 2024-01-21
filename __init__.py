a0ddb6db23781b87b721744c6a49c2bb3af87593
djkil <djkill2009@gmail.com>
2024-01-21 17:34:59 +0300

    ya disk

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
-from deepface import DeepFace
 import json 
+import os
 import cv2
 import numpy as np
 from PIL import Image
+from PIL.PngImagePlugin import PngInfo
 import torch
+import yadisk
+import folder_paths as comfy_paths
+from datetime import datetime
 
 # Hack: string type that is always equal in not equal comparisons
 class AnyType(str):
@@ -40,7 +44,8 @@ class AnyToString:
         res = str(any)
         return {"ui": {"tags": res}, "result": (res,)}
 
-class FaceAnalyze:
+
+class SaveYDrive:
 
     def __init__(self):
         pass
@@ -50,85 +55,76 @@ class FaceAnalyze:
 
         return {
             "required": {
-                "image": ("IMAGE",),
-                "backend": (['opencv', 
-                              'ssd', 
-                              'dlib', 
-                              'mtcnn', 
-                              'retinaface', 
-                              'mediapipe',
-                              'yolov8',
-                              'yunet',
-                              'fastmtcnn',], {}),
-                                        },
-        }
-
-    RETURN_TYPES = ("STRING",)
-    #RETURN_NAMES = ("image_output_name",)
-
-    FUNCTION = "test"
-
-    #OUTPUT_NODE = False
-
-    CATEGORY = "Facial"
-
-    def test(self, image, backend):
-        tensor = image*255
-        tensor = np.array(tensor, dtype=np.uint8)        
-        objs = DeepFace.analyze(tensor[0], actions=("age", "gender"), silent=False, enforce_detection=False, detector_backend = backend)
-        res = json.dumps(objs,)
-        print(res)
-        return {"ui": {"tags": res}, "result": (res,)}
-
-class FaceCompare:
-
-    def __init__(self):
-        pass
-    
-    @classmethod
-    def INPUT_TYPES(s):
-
-        return {
-            "required": {
-                "image1": ("IMAGE",),
-                "image2": ("IMAGE",),
-                "metric": (['euclidean_l2', 
-                              'cosine', 
-                              'euclidean',], {}),
+                "images": ("IMAGE", ),
+                "token": ("STRING", {"default": ""}),
+            },
+            "hidden": {
+                "prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"
             },
         }
 
-    RETURN_TYPES = ("BOOLEAN", "FLOAT", "STRING")
-    RETURN_NAMES = ("verified", "distance", "result")
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
 
-    FUNCTION = "test"
+    FUNCTION = "save_image"
 
-    #OUTPUT_NODE = False
+    OUTPUT_NODE = True
 
-    CATEGORY = "Facial"
+    CATEGORY = "image"
 
-    def test(self, image1, image2, metric):
-        tensor1 = image1*255
-        tensor1 = np.array(tensor1, dtype=np.uint8)        
-        tensor2 = image2*255
-        tensor2 = np.array(tensor2, dtype=np.uint8)        
-        objs = DeepFace.verify(tensor1[0], tensor2[0], distance_metric = metric)
-        res = json.dumps(objs,)
-        print(res)
-        return objs["verified"], objs["distance"], res
+    def save_image(self, images, token="", prompt=None, extra_pnginfo=None):
+        subfolder = datetime.today().strftime('%Y-%m-%d')
+        timef = datetime.today().strftime('%H:%M:%S.%f')[:-3]
+        client = yadisk.Client(token=token)
+        # Проверяет, валиден ли токен
+        print(client.check_token())    
+        # Получает общую информацию о диске
+        print(client.get_disk_info())
+        try:
+            client.mkdir("Comfy/"+subfolder)
+        except yadisk.exceptions.PathExistsError:
+            pass     
+        tmpdir = comfy_paths.get_temp_directory()
+        results = list()
+        counter = 0
+        for image in images:
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            metadata = PngInfo()
+            if prompt is not None:
+                metadata.add_text("prompt", json.dumps(prompt))
+            if extra_pnginfo is not None:
+                for x in extra_pnginfo:
+                    metadata.add_text(x, json.dumps(extra_pnginfo[x]))            
+            
+            file = f"{timef}_{counter}.png"
+            output = os.path.join(tmpdir, subfolder)
+            if not os.path.exists(output):
+                os.makedirs(output)
+            res = os.path.join(output, file)    
+            img.save(res, pnginfo=metadata, compress_level=4)
+            client.upload(res, os.path.join("Comfy/"+subfolder, file)) 
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": "temp"
+            })
+            counter += 1
+        print(results)
+        return { "ui": { "images": results } }      
+
 
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "FaceAnalyze": FaceAnalyze,
-    "FaceCompare": FaceCompare,
     "AnyToString": AnyToString,
+    "SaveYDrive": SaveYDrive,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "FaceAnalyze": "Facial Attribute Analysis",
-    "FaceCompare": "Face similarity",
     "AnyToString": "Any To String",
+    "SaveYDrive": "Save to Yandex drive",
 }
